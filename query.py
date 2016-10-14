4f4499e27d19531d78142ee7666a61f3e046c30f
jrottersman <rottersmanjake@gmail.com>
2016-10-13 18:46:15 -0700

    got select, filter and order working

--- query.py
+++ query.py
@@ -3,6 +3,10 @@ import os
 import sys
 import argparse
 
+from collections import defaultdict
+from pprint import pprint
+from operator import itemgetter
+
 def get_args():
     '''This functions parses and returns arguments passed in'''
 
@@ -14,7 +18,7 @@ def get_args():
     parser.add_argument(
             '-o', '--order', type=str, help='Columns to order by', required=False, default=None, nargs='*')
     parser.add_argument(
-            '-f', '--filter', type=str, help='Column and value to filter by', required=False, default=None, nargs='*')
+            '-f', '--filter', type=str, help='Column and value to filter by', required=False, default=None) 
 
     #gather all the arguments into an array
     args = parser.parse_args()
@@ -26,21 +30,56 @@ def get_args():
     return select, order, filter
 
 select, order, filter = get_args()
-print("select the following: [%s]\n" % select)
-print("ordered by: [%s]\n" % order)
-print("filtered withL %s\n" % filter)
-
-#def run():
- #   path = 'data/'
-#
- #   for fileName in os.listdir(path):
-  #      with open(fileName, 'w'):
-   #         select():
-
-#def init_select(*arg):
- #   for i in range(len(arg)):
 
 
-#def select(*arg):
- #   for i in range(len(arg)):
 
+def select_data(select, files):
+    #create a dict with our selected values as keys and lists to store our values
+    values = []
+   # path = 'data/'
+
+    #for fileName in os.listdir(path):
+     #   fileName = os.path.join(path, fileName)
+    for fileName in files:
+        l = {}
+        with open(fileName, 'r') as f:
+            json_data = json.load(f)
+            for i in select:
+                l[i] = json_data[i]
+        
+        values.append(l)
+
+    return values
+
+def filter_data(filter):
+    #split the strings in the list
+    #return the files that contain our true value
+    files = []
+    path = 'data/'
+
+    if filter == None:
+        for fileName in os.listdir(path):
+            fileName = os.path.join(path, fileName)
+            files.append(fileName)
+    else:
+
+        key, value = str(filter).split("=")
+
+        for fileName in os.listdir(path):
+            fileName = os.path.join(path, fileName)
+
+            with open(fileName, 'r') as f:
+                data = json.load(f)
+                if data[key] == value:
+                    files.append(fileName)
+        
+    return files
+        
+def order_data(data, order):
+    if order != None:
+        data = sorted(data, key = itemgetter(",".join(order)))
+    return data
+
+x = order_data(select_data(select, filter_data(filter)), order)
+print(x)
+#print(order_data(order, selector(select)))
